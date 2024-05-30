5405512779f3a6268172c975525ad5b2d8763dab
John Tönnäng <john_tonnang@hotmail.com>
2024-05-30 14:17:50 +0200

    Fix bug with studio dataset

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,14 +2,14 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
-import {studioProjectId, studioTitle} from './environment'
+import {studioProjectId, studioTitle, studioDataset} from './environment'
 
 export default defineConfig({
   name: 'default',
   title: studioTitle,
 
   projectId: studioProjectId,
-  dataset: 'studioDataset',
+  dataset: studioDataset,
 
   plugins: [structureTool(), visionTool()],
 
